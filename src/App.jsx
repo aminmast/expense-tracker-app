6aa0ce3ddc2c7b716c75ce8fc93230548e42776d
mohammad amin masteali <amast1375@gmail.com>
2022-02-11 22:06:02 +0330

    change title name

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import ExpeseApp from "./components/ExpeseApp/ExpenseApp";
 
 const App = () => {
+  document.title = "Expense Tracker App";
   return (
     <div className="body">
       <ExpeseApp />
